5ef543785393be73da76af443e1d194fba816bc3
arian <arian.maki@yahoo.com>
2025-10-03 22:40:36 +0200

    CheckOTPForm added

---
+++ src/features/authentication/CheckOTPForm.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import OTPInput from "react-otp-input";
+
+function CheckOTPForm() {
+  const [otp, setOtp] = useState(null);
+  return (
+    <form className="space-y-5">
+      <p className="text-slate-900 text-lg">
+        Enter the 6-digit code we sent to your phone
+      </p>
+      <OTPInput
+        value={otp}
+        onChange={setOtp}
+        numInputs={6}
+        renderSeparator={<span>-</span>}
+        renderInput={(props) => <input type="number" {...props} />}
+        containerStyle="flex gap-x-2 justify-center"
+        inputStyle={{
+          width: "2.5rem",
+          padding: "0.5rem 0.2rem",
+          border: "1px solid rgb(var(--color-primary-400))",
+          borderRadius: "0.5rem",
+        }}
+      />
+      <button className="w-full btn btn--primary">verify</button>
+    </form>
+  );
+}
+
+export default CheckOTPForm;
