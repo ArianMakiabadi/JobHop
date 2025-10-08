4ebce4573c6d6d1ab4752a18e345c4e520df40ca
arian <arian.maki@yahoo.com>
2025-10-08 22:15:23 +0200

    styles added to layout

--- src/UI/AppLayout.jsx
+++ src/UI/AppLayout.jsx
@@ -3,10 +3,12 @@ import { Outlet } from "react-router-dom";
 function AppLayout() {
   return (
     <div className="grid h-screen grid-rows-[auto_1fr] grid-cols-[15rem_1fr]">
-      <div className="bg-red-200 col-span-2">App header</div>
-      <div className="bg-red-300">App sidebar</div>
-      <div className="bg-blue-300">
-        <Outlet />
+      <div className="bg-red-200">App header</div>
+      <div className="bg-red-300 row-start-1 row-span-2">App sidebar</div>
+      <div className="bg-secondary-100 p-8 overflow-y-auto">
+        <div className="mx-auto max-w-screen-md flex flex-col gap-y-12">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
