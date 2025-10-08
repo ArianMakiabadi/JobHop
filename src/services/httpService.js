e8869ceb01e57df71f439888805eb81d447429b3
arian <arian.maki@yahoo.com>
2025-10-08 19:58:11 +0200

    renewing access token

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,9 +1,30 @@
 import axios from "axios";
+const BASE_URL = "http://localhost:5001/api";
 
 const app = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: BASE_URL,
   withCredentials: true,
 });
+
+// generating new access token if it expires during an action
+app.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    const originalConfig = err.config; //?The request on which the user got an error on
+    if (err.response.status === 401 && !originalConfig._retry) {
+      originalConfig._retry = true; // to avoid infinite loop
+      try {
+        const { data } = await axios.get(`${BASE_URL}/user/refresh-token`, {
+          withCredentials: true,
+        });
+        if (data) return app(originalConfig); //? Sending the request again after setting new access token
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }
+    return Promise.reject(err);
+  }
+);
 
 const http = {
   get: app.get,
