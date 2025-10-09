23d73234269723c855036c6cf547d5679c083074
arian <arian.maki@yahoo.com>
2025-10-09 16:06:50 +0200

    SideBar details added

--- src/UI/SideBar.jsx
+++ src/UI/SideBar.jsx
@@ -1,5 +1,43 @@
+import { HiCollection, HiHome } from "react-icons/hi";
+import { NavLink } from "react-router-dom";
+
 function SideBar() {
-  return <div className="bg-red-300 row-start-1 row-span-2">App sidebar</div>;
+  return (
+    <div className="bg-white row-start-1 row-span-2 p-4">
+      <ul className="flex flex-col gap-y-4">
+        <li>
+          <CustomNavLink to={"/employer/dashboard"}>
+            <HiHome />
+            <span>Home</span>
+          </CustomNavLink>
+        </li>
+        <li>
+          <CustomNavLink to={"/employer/projects"}>
+            <HiCollection />
+            <span>Projects</span>
+          </CustomNavLink>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default SideBar;
+
+function CustomNavLink({ children, to }) {
+  const navLinkClass =
+    "flex items-center gap-2 hover:bg-primary-100/50 hover:text-primary-900 px-2 py-1.5 rounded-xl transition-all duration-300";
+
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) =>
+        isActive
+          ? `${navLinkClass} bg-primary-100/50 text-primary-900`
+          : `${navLinkClass} text-secondary-600`
+      }
+    >
+      {children}
+    </NavLink>
+  );
+}
