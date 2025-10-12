ab10412307ba1cdffeb5a89b4fcd063c59cde94a
arian <arian.maki@yahoo.com>
2025-10-12 22:17:36 +0200

    using react-hook-form in input fields

--- src/UI/TextField.jsx
+++ src/UI/TextField.jsx
@@ -1,20 +1,32 @@
-function TextField({ label, name, onChange, placeholder, value }) {
+function TextField({
+  label,
+  name,
+  placeholder,
+  register,
+  validationSchema,
+  errors,
+  type = "text",
+  required,
+}) {
   return (
     <div>
       <div>
-        <label className="mb-1 text-slate-600" htmlFor={name}>
-          {label}
+        <label className="mb-1 text-secondary-700" htmlFor={name}>
+          {label} {required && <span className="text-error">*</span>}
         </label>
         <input
+          {...register(name, validationSchema)}
           id={name}
-          name={name}
-          value={value}
-          onChange={onChange}
           className="w-full textField__input"
           placeholder={placeholder}
-          type="text"
+          type={type}
           autoComplete="off"
         />
+        {errors && errors[name] && (
+          <span className="text-error block text-sm mt-0.5">
+            {errors[name]?.message}
+          </span>
+        )}
       </div>
     </div>
   );
