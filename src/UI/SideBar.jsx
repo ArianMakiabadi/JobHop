da3872da365347ae1bcd1ded90e62ab89cf72691
arian <arian.maki@yahoo.com>
2025-10-21 18:46:11 +0200

    cleanup: unused imports removed

--- src/UI/SideBar.jsx
+++ src/UI/SideBar.jsx
@@ -1,6 +1,3 @@
-import { HiCollection, HiHome } from "react-icons/hi";
-import { NavLink } from "react-router-dom";
-
 function SideBar({ children }) {
   return (
     <div className="bg-secondary-0 row-start-1 row-span-2 p-4">
