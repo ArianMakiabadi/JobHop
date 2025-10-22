484e42ddbbe5431a230e98f0e8483aa78f97441b
arian <arian.maki@yahoo.com>
2025-10-22 16:06:23 +0200

    landing page added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import CompleteProfile from "./pages/CompleteProfile";
 import NotFound from "./pages/NotFound";
-import Home from "./pages/Home";
 import EmployerDashboard from "./pages/EmployerDashboard";
 import Projects from "./pages/Projects";
 import Project from "./pages/Project";
@@ -20,6 +19,7 @@ import AdminLayout from "./features/admin/AdminLayout";
 import AdminDashboard from "./pages/AdminDashboard";
 import Users from "./pages/Users";
 import PendingApproval from "./pages/PendingApproval";
+import { Home } from "./pages/Home";
 
 const queryClient = new QueryClient();
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,76 @@
-function Home() {
-  return <div>Home</div>;
-}
+import { Stars } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { useEffect } from "react";
+import { FiArrowRight } from "react-icons/fi";
+import {
+  useMotionTemplate,
+  useMotionValue,
+  motion,
+  animate,
+} from "framer-motion";
+import { useNavigate } from "react-router-dom";
 
-export default Home;
+const COLORS_TOP = ["#ed05f2", "#6d1394", "#b530b8"];
+
+export const Home = () => {
+  const color = useMotionValue(COLORS_TOP[0]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    animate(color, COLORS_TOP, {
+      ease: "easeInOut",
+      duration: 10,
+      repeat: Infinity,
+      repeatType: "mirror",
+    });
+  }, []);
+
+  const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 0%, #020617 50%, ${color})`;
+  const border = useMotionTemplate`1px solid ${color}`;
+  const boxShadow = useMotionTemplate`0px 4px 24px ${color}`;
+
+  return (
+    <motion.section
+      style={{
+        backgroundImage,
+      }}
+      className="relative grid min-h-screen place-content-center overflow-hidden bg-gray-950 px-4 py-24 text-gray-200"
+    >
+      <div className="relative z-10 flex flex-col items-center">
+        <span className="mb-1.5 inline-block rounded-full bg-gray-600/50 px-3 py-1.5 text-xs">
+          Demo version!
+        </span>
+        <h1 className="max-w-3xl bg-gradient-to-br from-white to-gray-400 bg-clip-text text-center text-5xl font-medium leading-tight text-transparent sm:leading-tight md:text-7xl md:leading-tight">
+          JobHop!
+        </h1>
+        <p className="my-3 md:my-6 max-w-xl text-center text-sm leading-relaxed md:text-lg md:leading-relaxed">
+          The best place to connect freelancers with employers. Browse projects,
+          showcase your skills, or hire top talent; all in one place.
+        </p>
+        <motion.button
+          onClick={() => navigate("/auth")}
+          style={{
+            border,
+            boxShadow,
+          }}
+          whileHover={{
+            scale: 1.015,
+          }}
+          whileTap={{
+            scale: 0.985,
+          }}
+          className="group text-sm md:text-lg relative flex w-fit items-center gap-1.5 rounded-full bg-gray-950/10 px-4 py-2 text-gray-50 transition-colors hover:bg-gray-950/50"
+        >
+          Start for free
+          <FiArrowRight className="transition-transform group-hover:-rotate-45 group-active:-rotate-12" />
+        </motion.button>
+      </div>
+
+      <div className="absolute inset-0 z-0">
+        <Canvas>
+          <Stars radius={50} count={2500} factor={4} fade speed={2} />
+        </Canvas>
+      </div>
+    </motion.section>
+  );
+};
