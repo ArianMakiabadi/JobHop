689923a1878f13fed9f611a5aead8a4578772798
arian <arian.maki@yahoo.com>
2025-10-23 00:38:30 +0200

    input field changed

--- src/features/authentication/SendOTPForm.jsx
+++ src/features/authentication/SendOTPForm.jsx
@@ -1,13 +1,9 @@
-import TextField from "../../UI/TextField";
 import Loading from "../../UI/Loading";
+import { Controller } from "react-hook-form";
+import { PhoneInput } from "react-international-phone";
+import "react-international-phone/style.css";
 
-function SendOTPForm({
-  isSendingOtp,
-  onSubmit,
-  register,
-  validationSchema,
-  errors,
-}) {
+function SendOTPForm({ isSendingOtp, onSubmit, control, validationSchema }) {
   return (
     <div className="border shadow-md px-10 pb-8 pt-10 w-[28rem] mx-auto rounded-3xl">
       <h2 className="font-bold text-secondary-700 text-3xl text-center pb-2">
@@ -17,12 +13,23 @@ function SendOTPForm({
         Enter your phone number to continue
       </p>
       <form className="space-y-4" onSubmit={onSubmit}>
-        <TextField
-          label="Phone number"
+        <Controller
           name="phoneNumber"
-          register={register}
-          errors={errors}
-          validationSchema={validationSchema}
+          control={control}
+          defaultValue=""
+          rules={validationSchema}
+          render={({ field }) => (
+            <div>
+              <PhoneInput
+                value={field.value}
+                onChange={(value) => field.onChange(value)}
+                country="de"
+                defaultCountry="de"
+                inputStyle={{ width: "100%" }}
+                inputProps={{ name: "phoneNumber", required: true }}
+              />
+            </div>
+          )}
         />
         <div>
           {isSendingOtp ? (
